0223271924cc7a3af29b29eae607080f0778bc5e
riverajuanda <juan_david_rivera@hotmail.com>
2025-05-23 15:24:53 -0500

    Refactor artista class: update properties and methods for database interactions

--- discotienda/class/c_artista.php
+++ discotienda/class/c_artista.php
@@ -1,89 +1,94 @@
 <?php
-include_once("basedatos.php");
-class banco extends basedatos
+include_once "basedatos.php";
+//clase artista
+
+class artista extends basedatos
 {
-    public $codigo;
+    public $artistaID;
     public $nombre;
-    public $cod_transaccion;
-    function __construct($codigo = NULL, $nombre = NULL, $cod_transaccion = NULL)
+    public $nacionalidad;
+    public function __construct($artistaID = 'NULL', $nombre = 'NULL', $nacionalidad = 'NULL')
     {
-        $this->codigo = $codigo;
+        $this->artistaID = $artistaID;
         $this->nombre = $nombre;
-        $this->cod_transaccion = $cod_transaccion;
+        $this->nacionalidad = $nacionalidad;
     }
-
-    public function getCodigo()
-    {
-        return $this->codigo;
+    //metodo get
+    public function getartistaID()    {
+        return $this->artistaID;
     }
-
-    public function getNombre()
-    {
+    public function getNombre()    {
         return $this->nombre;
     }
 
-    public function getCodTransaccion()
-    {
-        return $this->cod_transaccion;
+    //public function getCodTransaccion()
+    //{
+    // return $this->cod_transaccion;
+    //}
+    //metodo set
+    public function setartistaID($artistaID)    {
+        $this->artistaID = $artistaID;
     }
-
-    public function setCodigo($codigo)
-    {
-        return $this->codigo = $codigo;
+    public function setNombre($nombre)    {
+        $this->nombre = $nombre;
     }
-
-    public function setNombre($nombre)
-    {
-        return $this->nombre = $nombre;
-    }
-
-    public function setCodTransaccion($cod_transaccion)
-    {
-        return $this->cod_transaccion = $cod_transaccion;
-    }
-
-    public function insertar()
-    {
-        $sql = sprintf("INSERT INTO banco (codigo,nombre,cod_transaccion) VALUES ('%s', '%s', '%s')", $this->codigo, $this->nombre, $this->cod_transaccion);
+    //public function setCodTransaccion($cod_transaccion)
+    //{
+    // $this->cod_transaccion = $cod_transaccion;
+    //}
+    public function insertar()    {
+        $sql = sprintf(        "INSERT INTO artista (artistaID, nombre, nacionalidad) VALUES ('%s', '%s', '%s')",
+            $this->artistaID,
+            $this->nombre,
+            $this->nacionalidad
+        );
         $this->conectar();
-        $this->ejecutarSQL($sql);
+        //echo $sql;
+        $this->ejecutarSQL(sql: $sql);
         $this->desconectar();
     }
-
-    public function listar()
-    {
-        $sql = "SELECT * FROM banco ORDER BY nombre ASC";
+    public function listar()    {
+        $sql = "select * FROM artista
+ ORDER BY nombre ASC";
         $this->conectar();
-        $this->ejecutarSQL($sql);
+        $this->ejecutarSQL(sql: $sql);
         $res = $this->cargarTodo();
         $this->desconectar();
         return $res;
     }
-
-    public function consultar()
-    {
-        $sql = sprintf("SELECT * FROM banco WHERE codigo = %s", $this->codigo);
+    public function consultar()    {
+        $sql = sprintf(
+            "SELECT * FROM artista
+ WHERE artistaID
+ = '%s'",
+            $this->artistaID
+        );
         $this->conectar();
         $this->ejecutarSQL($sql);
         $res = $this->cargarRegistro();
         $this->desconectar();
         $this->nombre = $res["nombre"];
-        $this->cod_transaccion = $res["cod_transaccion"];
+        $this->nacionalidad = $res["nacionalidad"];
     }
 
-    public function eliminar()
-    {
-        $sql = sprintf("DELETE FROM banco WHERE codigo = %s", $this->codigo);
+    public function eliminar()    {
+        $sql = sprintf("DELETE FROM artista WHERE artistaID = '%s'",$this->artistaID        );
         $this->conectar();
         $this->ejecutarSQL($sql);
         $this->desconectar();
     }
-
-    public function actualizar()
-    {
-        $sql = sprintf("UPDATE banco SET codigo = '%s', nombre = '%s', cod_transaccion = '%s' WHERE codigo = '%s'", $this->codigo, $this->nombre, $this->cod_transaccion, $this->codigo);
-        $this->conectar();
-        $this->ejecutarSQL($sql);
-        $this->desconectar();
-    }
+public function actualizar()
+{
+    $sql = sprintf(
+        "UPDATE artista
+ SET nombre = '%s', nacionalidad = '%s' WHERE artistaID
+ = '%s'",
+        $this->nombre,
+        $this->nacionalidad,
+        $this->artistaID
+    );
+    $this->conectar();
+    $this->ejecutarSQL($sql);
+    $this->desconectar();
+}
 }
